0131545ecffe6d9eb920435e859c5d05bb4ccc04
Mati-Muli <m.mulewicz89@gmail.com>
2026-05-06 17:28:01 +0200

    Poprawki

---
+++ Lab4/BlazorApp1/Program.cs
@@ -0,0 +1,29 @@
+using BlazorApp1.Components;
+using Microsoft.AspNetCore.Authentication.Certificate;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddAuthentication(
+CertificateAuthenticationDefaults.AuthenticationScheme)
+      .AddCertificate();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
